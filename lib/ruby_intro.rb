897bb309bba2884dffb358c86575189033dcf753
tylerfallon <tylermfallon@gmail.com>
2017-06-05 11:38:46 -0400

    all parts completed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -37,5 +37,13 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  def initialize(isbn, price)
+    raise ArgumentError if isbn == '' || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  def price_as_string
+    format "$%.2f", price
+  end
 end
